1e1faaee57386d65ae2e86867f8836b562fc1083
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-07-31 16:47:01 +0100

    Change dictionary key case

--- utilities.py
+++ utilities.py
@@ -9,20 +9,20 @@ def parse_alert(text):
         return None
 
     data = {
-        "type": "UNKNOWN",
-        "host": None,
-        "service": None,
-        "state": None,
+        "Type": "UNKNOWN",
+        "Host": None,
+        "Service": None,
+        "State": None,
     }
 
     matches = [type for type in config.ALERT_TYPES if type in lines[0].upper()]
-    data["type"] = matches[0] if matches else "UNKNOWN"
+    data["Type"] = matches[0] if matches else "UNKNOWN"
 
     for i, line in enumerate(lines[1:], start=1):
         if line.startswith(config.ALERT_FIELDS):
             key, value = [text.strip() for text in line.split(":", 1)]
-            data[key.lower()] = value
+            data[key.title()] = value
         elif "Additional Info" in line:
-            data["message"] = "\n".join(lines[i + 1 :]).strip()
+            data["Message"] = "\n".join(lines[i + 1 :]).strip()
 
     return data
